0c34fa533ed215192442da0d7ea388a99c2b90d0
Cvargas17 <christianvargas2002@gmail.com>
2021-11-18 21:48:24 -0600

    lol

--- Discord.py
+++ Discord.py
@@ -1,14 +1,16 @@
 import discord
 from discord.ext import commands
-import music
+from discord import FFmpegPCMAudio
+import Musica
 
-cogs = [musica]
+cogs = [Musica]
 
-client = commands.Bot(command_prefix="!", intents = discord.Intents)
+intents = discord.Intents().all()
+client = commands.Bot(command_prefix="!", intents = discord.Intents.all())
 
 for i in range(len(cogs)):
     cogs[i].setup(client)
 
 
 
-client.run("OTEwNzIxNzkyOTA4MzQ5NDUw.YZW9ww.oO2TBV5VGymevX4E7ECdy54NhzQ")
+client.run("OTEwNzIxNzkyOTA4MzQ5NDUw.YZW9ww.jsp6z_BGYWBw8mIrmvjvJoBIVkc")
--- Musica.py
+++ Musica.py
@@ -1,10 +1,11 @@
 import discord
 from discord.ext import commands
+from discord import FFmpegPCMAudio
 import youtube_dl
 
 class musica(commands.Cog):
-    def__init__(self, client)
-    self.client = client
+    def __init__(self, client):
+     self.client = client
 
     @commands.command()
     async def join(self,ctx):
@@ -20,19 +21,17 @@ class musica(commands.Cog):
     async def disconnect(self,ctx):
         await ctx.voice_client.disconnect()
 
-    @commands.commmand()
+    @commands.command()
     async def play(self,ctx,url):
         ctx.voice_client.stop()
-        FFMPEG_OPTIONS = {}
+        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
         YDL_OPTIONS = {'format' : "bestaudio"}
         vc = ctx.voice_client
 
-        with youtube_dl.YoutubeDL(YDL-OPTIONS) as ydl:
-            info = ydl.extract_info(url, download=false)
+        with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url, download = False)
             url2 = info['formats'][0]['url']
-            source = await discord.FFmegOpusAudio.from_probe(url2,
-            **FFMEPG_OPTIONS)
-            vc.play(source)
+            ctx.voice_client.play(vc.play(discord.FFmpegPCMAudio(executable = r"C:\Users\Comercia\Desktop\ffmpeg-2021-11-15-git-9e8cdb24cd-full_build\bin\ffmpeg.exe ", source = url2)))
 
 
     @commands.command()
@@ -49,3 +48,4 @@ class musica(commands.Cog):
 
 def setup(client):
     client.add_cog(musica(client))
+
