df08b2a58da2057492ec2fbbdd68fbcb6f18a595
Armando Calderon <93158352+Arman3107@users.noreply.github.com>
2021-11-23 16:37:02 -0600

    Mejora en audio
    Mejora para el ytb1

--- Musica.py
+++ Musica.py
@@ -8,15 +8,21 @@ class musica(commands.Cog):
      self.client = client
 
     @commands.command()
+    
     async def join(self,ctx):
-        if ctx.author.voice is None:
-            await ctx.send("Canal Vacio!")
-        voice_channel = ctx.author.voice.channel
-        if ctx.voice_client is None:
-            await voice_channel.connect()
-        else:
-            await ctx.voice_client.move_to(voice_channel)
-
+        try:
+            if ctx.author.voice is None:
+                await ctx.send("Canal Vacio!")
+                voice_channel = ctx.author.voice.channel
+            if ctx.voice_client is None:
+                await voice_channel.connect()
+            else:
+                await ctx.voice_client.move_to(voice_channel)
+        except PermissionError: 
+            await ctx.send("El bot se encuentra en otro canal de voz")
+        return
+    
+       
     @commands.command()
     async def disconnect(self,ctx):
         await ctx.voice_client.disconnect()
@@ -32,7 +38,15 @@ class musica(commands.Cog):
             info = ydl.extract_info(url, download = False)
             url2 = info['formats'][0]['url']
             ctx.voice_client.play(vc.play(discord.FFmpegPCMAudio(executable = r"C:\Users\Comercia\Desktop\ffmpeg-2021-11-15-git-9e8cdb24cd-full_build\bin\ffmpeg.exe ", source = url2)))
-
+            
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+   }
 
     @commands.command()
     async def pause(self,ctx):
