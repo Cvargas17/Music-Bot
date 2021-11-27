af28e667e2a6216feb25db5e2ca7f466e3ff69d2
Armando Calderon <93158352+Arman3107@users.noreply.github.com>
2021-11-27 13:46:43 -0600

    Documentación de código principal

--- Musica.py
+++ Musica.py
@@ -2,10 +2,12 @@ import discord
 from discord.ext import commands
 from discord import FFmpegPCMAudio
 import youtube_dl
+#Se importan las librerias a utilizar en el codigo
 
 class musica(commands.Cog):
     def __init__(self, client):
      self.client = client
+#Se define la clase musica
 
     @commands.command()
     async def join(self,ctx):
@@ -16,11 +18,12 @@ class musica(commands.Cog):
             await voice_channel.connect()
         else:
             await ctx.voice_client.move_to(voice_channel)
-
+#Creacion del comando !join
+            
     @commands.command()
     async def disconnect(self,ctx):
         await ctx.voice_client.disconnect()
-
+#Creacion del comando !disconnect
     @commands.command()
     async def play(self,ctx,url):
         ctx.voice_client.stop()
@@ -33,19 +36,20 @@ class musica(commands.Cog):
             url2 = info['formats'][0]['url']
             ctx.voice_client.play(vc.play(discord.FFmpegPCMAudio(executable = r"C:\Users\Comercia\Desktop\ffmpeg-2021-11-15-git-9e8cdb24cd-full_build\bin\ffmpeg.exe ", source = url2)))
 
-
+#Creacion del comando !play
     @commands.command()
     async def pause(self,ctx):
         await ctx.voice_client.pause()
         await ctx.send("Musica Pausada")
+#Creacion del comando !pause
 
     @commands.command()
     async def resume(self,ctx):
         await ctx.voice_client.resume()
         await ctx.send("Sigue la musica")
-
+#Creacion del comando !resume
 
 
 def setup(client):
     client.add_cog(musica(client))
-
+#Se define el cliente para poder correr el token de la aplicación
